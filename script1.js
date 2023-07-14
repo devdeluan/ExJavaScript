32e11e27c86d860f2c9c8d5e2caf4f57778759f4
Deluan26 <deluan19951@gmail.com>
2023-07-14 16:56:52 -0300

    ok

---
+++ script1.js
@@ -0,0 +1,12 @@
+console.log("Exercício 1");
+
+let prompt = require("prompt-sync")();
+
+let usuario = prompt("Digite o Usuário: ");
+let senha = prompt("Digite a senha: ");
+
+if (usuario == admin && senha == senha123){
+console.log ("Login aprovado!")
+} else {
+    console.log ("Login não aprovado!!")
+}
